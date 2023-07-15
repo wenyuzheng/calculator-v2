effa53b422c8acd8c8a6866d69e200b51d88db4d
Wenyu Zheng <zhengwy229@gmail.com>
2023-07-15 13:49:03 +0100

    Style buttons

--- src/components/Key.js
+++ src/components/Key.js
@@ -4,7 +4,11 @@ const Key = ({ value, onClick }) => {
   const { id, text } = value;
 
   return (
-    <Button id={id} onClick={onClick} className="btn btn-lg w-100 h-100">
+    <Button
+      id={id}
+      onClick={onClick}
+      className="btn btn-lg w-100 h-100 rounded-0 border"
+    >
       {text}
     </Button>
   );
